9e782e8e11e827570992f9280a17f6372567973b
Keko Buzz <macbookpro@Keko-Buzzs-MacBook-Pro.local>
2017-07-18 13:47:02 -0500

    first commit

---
+++ modules/Authors/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+GENDER = (
+	("M","Masculino"),
+	("F","Femenino"),
+	)
+
+#se agrego despues para seleccionar el genero literario
+"""LIT_GENRE= (
+	("sf","science fiction"),
+	("dr","drama"),
+	("ft","fantasy"),
+	("bg","biography"),
+	("ht","history"),
+	("ot","others")
+	)
+"""
+
+class Author(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	nacionality = models.CharField(max_length=50)
+	biography = models.TextField()
+	gender = models.CharField(max_length=20, choices=GENDER)
+	#lit_genre = models.CharField(max_length=80, choices=LIT_GENRE)
+	age = models.IntegerField(max_length=3)
+	is_alive = models.BooleanField(default=True)
+
+
+
+
+
